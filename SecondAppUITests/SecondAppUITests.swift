492509158071af450f78fd04eaf6dcf9432e20e7
nach plan <nachplan@nachs-imac.local>
2016-10-13 10:28:07 +0200

    weniger aaskommentiert

--- SecondAppUITests/SecondAppUITests.swift
+++ SecondAppUITests/SecondAppUITests.swift
@@ -9,7 +9,7 @@
 import XCTest
 
 class SecondAppUITests: XCTestCase {
-    /*
+    
     override func setUp() {
         super.setUp()
         
@@ -27,7 +27,7 @@ class SecondAppUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+    /*
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
